fd3bbe39209f4d0a1be65fdfc4d209c974a25e54
elijahred23 <elijahred23@gmail.com>
2024-03-16 05:12:00 -0500

    Eli-4: added full house isHand function

--- src/utils/winningHands.jsx
+++ src/utils/winningHands.jsx
@@ -22,7 +22,7 @@ const numMatchesPoints = (cards, numMatches)=> {
         let rank = getRank(pictureType);
         if (currentSuitCount) {
             if(currentSuitCount[pictureType] === numMatches){
-                points += (rank * 100); 
+                points += currentSuitCount[pictureType] * (rank * 100); 
             }  
             else {
                 points += (rank * 10); 
@@ -33,6 +33,7 @@ const numMatchesPoints = (cards, numMatches)=> {
     return hasPair ? points : 0;
 }
 
+
 const winningHands = {
     "pair": {
         value: 1,
@@ -41,7 +42,38 @@ const winningHands = {
         }
     },
     "two pair": {
-        value: 2
+        value: 2,
+        isHand(cards){
+            let suitCount = {};
+            let points = 0;
+            let matchingSuits = [];
+        
+            for (const pictureType of cards) {
+                if (suitCount[pictureType]) {
+                    suitCount[pictureType]++;
+                } else {
+                    suitCount[pictureType] = 1;
+                }
+            }
+        
+            matchingSuits = Object.keys(suitCount).filter(suit => suitCount[suit] === 2);
+            const hasPair = matchingSuits?.length > 1;
+        
+            for (let pictureType of pictureTypes) {
+                let currentSuitCount = suitCount[pictureType];
+                let rank = getRank(pictureType);
+                if (currentSuitCount) {
+                    if(currentSuitCount[pictureType] === 2){
+                        points += currentSuitCount[pictureType] * (rank * 100); 
+                    }  
+                    else {
+                        points += (rank * 10); 
+                    }
+                }
+            }
+        
+            return hasPair ? points : 0;
+        }
     },
     "three of a kind": {
         value: 3,
@@ -49,7 +81,55 @@ const winningHands = {
             return numMatchesPoints(cards, 3);
         }
     },
-    "full house": 4,
+    "full house": {
+        value: 4,
+        isHand(cards){
+            let suitCount = {};
+            let points = 0;
+            let matchingSuits = [];
+        
+            for (const pictureType of cards) {
+                if (suitCount[pictureType]) {
+                    suitCount[pictureType]++;
+                } else {
+                    suitCount[pictureType] = 1;
+                }
+            }
+        
+            const hasFullHouse = false;
+            const hasThree = false;
+            const hasTwo = false;
+            
+            matchingSuits = Object.keys(suitCount).filter(suit => {
+                if(suitCount[suit] === 2){
+                    hasTwo = true;
+                }
+                else if(suitCount[suit] === 3){
+                    hasThree = true;
+                }
+            });
+            hasFullHouse = hasThree && hasTwo; 
+        
+
+            for (let pictureType of pictureTypes) {
+                let currentSuitCount = suitCount[pictureType];
+                let rank = getRank(pictureType);
+                if (currentSuitCount) {
+                    if(currentSuitCount[pictureType] === 2){
+                        points += currentSuitCount[pictureType] * (rank * 100); 
+                    }  
+                    else if(currentSuitCount[pictureType] === 3){
+                        points += currentSuitCount[pictureType] * (rank * 200); 
+                    }  
+                    else {
+                        points += (rank * 10); 
+                    }
+                }
+            }
+        
+            return hasFullHouse ? points : 0;
+        }
+    },
     "four of a kind": {
         value: 5,
         isHand(cards){
@@ -62,7 +142,7 @@ const winningHands = {
             return numMatchesPoints(cards, 5);
         },
     },
-    winningHandsList: ["five of a kind", "four of a kind", "three of a kind", "pair"],
+    winningHandsList: ["five of a kind", "full house", "four of a kind", "three of a kind","two pair", "pair"],
     determineHand(cards){
         for(let hand of this.winningHandsList){
             let points = this[hand].isHand(cards);
